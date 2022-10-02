8c217f40f7ef0f56631972f90476241dded02ac8
최현정 <hjchoi.dev@gmail.com>
2022-10-02 16:41:04 +0900

    :sparkles: Bottom Navigation 완성

--- app/src/main/java/com/hyunjung/bottomnavigation/MainActivity.kt
+++ app/src/main/java/com/hyunjung/bottomnavigation/MainActivity.kt
@@ -2,10 +2,26 @@ package com.hyunjung.bottomnavigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.hyunjung.bottomnavigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 네비게이션 호스트
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+
+        // 네이게이션 컨트롤러
+        val navController = navHostFragment.navController
+
+        // 바인딩
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
     }
 }
